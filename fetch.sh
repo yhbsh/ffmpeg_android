65727bfbb0b64533f4af97c364202ddf371b2ad7
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-04-29 21:49:02 +0300

    remove libiconv from build

--- fetch.sh
+++ fetch.sh
@@ -8,8 +8,3 @@ if [ ! -d ffmpeg-7.0 ]; then
   rm ffmpeg-7.0.tar.xz
 fi
 
-if [ ! -d libiconv-1.17 ]; then
-  wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz
-  tar fvx libiconv-1.17.tar.gz
-  rm libiconv-1.17.tar.gz
-fi
