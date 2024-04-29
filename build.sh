ad3cf37ddddab7de0ab3984641e40c7cbfb45bd2
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-04-29 21:51:36 +0300

    fix some things and remove libiconv

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 TARGET_ARCH="aarch64"
-API_LEVEL="30"
+API_LEVEL="21"
 NDK_VERSION="26.3.11579264"
 
 SDK_ROOT="$HOME/Library/Android/sdk"
@@ -21,31 +21,10 @@ CXX="$TOOLCHAINS/bin/$TARGET_ARCH-linux-android${API_LEVEL}-clang++"
 CFLAGS="-static -Os -fPIC -march=armv8-a -ffunction-sections -fdata-sections"
 LDFLAGS="-L$SYSROOT/usr/lib/$TARGET_ARCH-linux-android/$API_LEVEL -lc -Wl,--gc-sections"
 
-CROSS_PREFIX="$TOOLCHAINS/bin/$TARGET_ARCH-linux-android$API_LEVEL-"
+
 
 FFMPEG_SOURCE_DIR="$PWD/ffmpeg-7.0"
 FFMPEG_BUILD_DIR="$PWD/ffmpeg-7.0-android-$TARGET_ARCH-$API_LEVEL"
-
-LIBICONV_SOURCE_DIR="$PWD/libiconv-1.17"
-LIBICONV_BUILD_DIR="$PWD/libiconv-1.17-android-$TARGET_ARCH-$API_LEVEL"
-
-cd $LIBICONV_SOURCE_DIR
-./configure \
-  --host="$TARGET_ARCH-linux-android" \
-  --prefix="$LIBICONV_BUILD_DIR" \
-  --enable-static \
-  --disable-shared \
-  --with-sysroot="$SYSROOT" \
-  CC="$CC" \
-  CXX="$CXX" \
-  AR="$LLVM_AR" \
-  NM="$LLVM_NM" \
-  RANLIB="$LLVM_RANLIB" \
-  STRIP="$LLVM_STRIP" \
-  CFLAGS="$CFLAGS" \
-  LDFLAGS="$LDFLAGS"
-make -j$(nproc)
-make install -j$(nproc)
 
 cd $FFMPEG_SOURCE_DIR
 ./configure \
@@ -59,26 +38,17 @@ cd $FFMPEG_SOURCE_DIR
   --prefix="$FFMPEG_BUILD_DIR" \
   --extra-cflags="$CFLAGS" \
   --extra-ldflags="$LDFLAGS" \
-  --disable-everything \
-  --enable-decoder=h264 \
-  --enable-decoder=mpeg4 \
-  --enable-parser=h264 \
-  --enable-parser=mpeg4 \
-  --enable-demuxer=mov \
-  --enable-demuxer=mpegvideo \
-  --enable-protocol=file \
-  --enable-avcodec \
-  --enable-avformat \
-  --enable-avutil \
   --enable-static \
   --disable-shared \
+  --disable-programs \
+  --disable-iconv \
   --disable-debug \
   --disable-doc \
   --disable-symver \
-  --enable-gpl \
   --ar="$LLVM_AR" \
   --nm="$LLVM_NM" \
   --ranlib="$LLVM_RANLIB" \
   --strip="$LLVM_STRIP"
+
 make -j$(nproc)
 make install -j$(nproc)
