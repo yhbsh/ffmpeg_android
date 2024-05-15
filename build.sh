2d4c0c15bef5ff380d01e05caacc4699cf526d4e
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-05-16 01:59:13 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -xe
+set -exuo pipefail
 
 TARGET_ARCH="aarch64"
-API_LEVEL="21"
+API_LEVEL="34"
 NDK_VERSION="26.3.11579264"
 
 SDK_ROOT="$HOME/Library/Android/sdk"
@@ -15,51 +15,64 @@ LLVM_AR="$TOOLCHAINS/bin/llvm-ar"
 LLVM_NM="$TOOLCHAINS/bin/llvm-nm"
 LLVM_RANLIB="$TOOLCHAINS/bin/llvm-ranlib"
 LLVM_STRIP="$TOOLCHAINS/bin/llvm-strip"
-YASM="$TOOCHAINS/bin/yasm"
 
 CROSS_PREFIX="$TOOLCHAINS/bin/$TARGET_ARCH-linux-android"
 CC="${CROSS_PREFIX}${API_LEVEL}-clang"
 CXX="${CROSS_PREFIX}${API_LEVEL}-clang++"
-CFLAGS="-fPIC -Wl,-Bsymbolic -Os -fPIE -pie -static -fPIC"
-LDFLAGS="-L$SYSROOT/usr/lib/$TARGET_ARCH-linux-android/$API_LEVEL -lc -Wl, --verbose"
+CFLAGS="-Os -fPIC -ffunction-sections -fdata-sections"
 
 FFMPEG_SOURCE_DIR="$PWD/ffmpeg"
 FFMPEG_BUILD_DIR="$PWD/ffmpeg-android-$TARGET_ARCH-$API_LEVEL"
 
-cd $FFMPEG_SOURCE_DIR
+mkdir -p "$FFMPEG_BUILD_DIR"
+
+cd "$FFMPEG_SOURCE_DIR"
 ./configure \
-  --cc="$CC" \
-  --cxx="$CXX" \
-  --arch="$TARGET_ARCH" \
-  --target-os=android \
-  --yasmexe=$NDK/prebuilt/darwin-x86_64/bin/yasm \
   --prefix="$FFMPEG_BUILD_DIR" \
+  --target-os=android \
+  --arch="$TARGET_ARCH" \
   --cross-prefix="$CROSS_PREFIX" \
   --sysroot="$SYSROOT" \
-  --extra-cflags="$CFLAGS" \
-  --extra-ldflags="$LDFLAGS" \
-  --enable-pic \
-  --enable-cross-compile \
-  --enable-neon \
-  --enable-asm \
-  --enable-static \
-  --enable-inline-asm \
-  --disable-shared \
-  --disable-programs \
-  --disable-iconv \
-  --disable-debug \
-  --disable-symver \
-  --disable-doc \
-  --disable-avx \
-  --disable-htmlpages \
-  --disable-manpages \
-  --disable-podpages \
-  --disable-txtpages \
+  --cc="$CC" \
+  --cxx="$CXX" \
   --ar="$LLVM_AR" \
   --nm="$LLVM_NM" \
   --ranlib="$LLVM_RANLIB" \
-  --strip="$LLVM_STRIP"
+  --strip="$LLVM_STRIP" \
+  --enable-cross-compile \
+  --enable-pic \
+  --enable-static \
+  --enable-small \
+  --disable-shared \
+  --disable-doc \
+  --disable-debug \
+  --disable-symver \
+  --disable-encoders \
+  --disable-decoders \
+  --disable-muxers \
+  --disable-demuxers \
+  --disable-protocols \
+  --disable-filters \
+  --disable-avdevice \
+  --disable-avformat \
+  --disable-swscale \
+  --disable-avfilter \
+  --disable-postproc \
+  --disable-everything \
+  --enable-decoder=h264 \
+  --enable-decoder=aac \
+  --enable-decoder=mpeg4 \
+  --enable-parser=aac \
+  --enable-parser=h264 \
+  --extra-cflags="$CFLAGS" \
+
+read -p "Compile FFmpeg? (y/N): " confirm
+if [[ $confirm != [yY] ]]; then
+  echo "Compilation aborted."
+  exit 1
+fi
 
 make clean
-make -j$(nproc)
-make install -j$(nproc)
+make install -j
+
+echo "FFmpeg compilation completed successfully."
