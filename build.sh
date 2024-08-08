6d299af9733ef7c607ba071bcd4fbab1db519377
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-08-08 01:16:30 +0100

    simplify build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xe
-
 TARGET_ARCH="aarch64"
 API_LEVEL="34"
 NDK_VERSION="26.3.11579264"
@@ -20,65 +18,31 @@ CROSS_PREFIX="$TOOLCHAINS/bin/$TARGET_ARCH-linux-android"
 CC="${CROSS_PREFIX}${API_LEVEL}-clang"
 CXX="${CROSS_PREFIX}${API_LEVEL}-clang++"
 
-FFMPEG_SOURCE_DIR="$PWD/ffmpeg-7.0.1"
-FFMPEG_BUILD_DIR="$PWD/ffmpeg-android-$TARGET_ARCH-$API_LEVEL"
+cd "./ffmpeg-7.0.1"
 
-mkdir -p "$FFMPEG_BUILD_DIR"
-
-cd "$FFMPEG_SOURCE_DIR"
-./configure                                                                                                           \
-    --prefix="$FFMPEG_BUILD_DIR"                                                                                      \
-    --enable-cross-compile                                                                                            \
-    --cross-prefix="$CROSS_PREFIX"                                                                                    \
-    --target-os=android                                                                                               \
-    --arch="$TARGET_ARCH"                                                                                             \
-    --sysroot="$SYSROOT"                                                                                              \
-    --stdc=c11                                                                                                        \
-    --cc="$CC"                                                                                                        \
-    --cxx="$CXX"                                                                                                      \
-    --ar="$LLVM_AR"                                                                                                   \
-    --nm="$LLVM_NM"                                                                                                   \
-    --ranlib="$LLVM_RANLIB"                                                                                           \
-    --strip="$LLVM_STRIP"                                                                                             \
-    --pkg-config=pkg-config                                                                                           \
-    --disable-armv5te                                                                                                 \
-    --disable-armv6                                                                                                   \
-    --disable-armv6t2                                                                                                 \
-    --enable-lto                                                                                                      \
-    --enable-pic                                                                                                      \
-    --enable-small                                                                                                    \
-    --disable-autodetect                                                                                              \
-    --disable-shared                                                                                                  \
-    --disable-debug                                                                                                   \
-    --disable-programs                                                                                                \
-    --disable-logging                                                                                                 \
-    --disable-doc                                                                                                     \
-    --disable-everything                                                                                              \
-    --disable-zlib                                                                                                    \
-    --disable-avfilter                                                                                                \
-    --disable-swscale                                                                                                 \
-    --disable-swresample                                                                                              \
-    --disable-decoders                                                                                                \
-    --disable-encoders                                                                                                \
-    --disable-bsfs                                                                                                    \
-    --disable-muxers                                                                                                  \
-    --disable-demuxers                                                                                                \
-    --disable-parsers                                                                                                 \
-    --disable-hwaccels                                                                                                \
-    --disable-protocols                                                                                               \
-    --disable-indevs                                                                                                  \
-    --disable-outdevs                                                                                                 \
-    --enable-mediacodec                                                                                               \
-    --enable-jni                                                                                                      \
-    --enable-protocol=udp,tcp,http,rtmp                                                                               \
-    --enable-demuxer=mov                                                                                              \
-    --enable-parser=h264                                                                                              \
-    --enable-decoder=h264,h264_mediacodec                                                                             \
-    --enable-indev=android_camera                                                                                     \
-
-{ set +xe; } 2>/dev/null
-read -n 1 -s
-set -xe
-
-make -j100
-make install
+./configure \
+    --prefix="$PWD/../android_build" \
+    --enable-cross-compile \
+    --cross-prefix="$CROSS_PREFIX" \
+    --target-os=android \
+    --arch="$TARGET_ARCH" \
+    --sysroot="$SYSROOT" \
+    --stdc=c11 \
+    --cc="$CC" \
+    --cxx="$CXX" \
+    --ar="$LLVM_AR" \
+    --nm="$LLVM_NM" \
+    --ranlib="$LLVM_RANLIB" \
+    --strip="$LLVM_STRIP" \
+    --pkg-config=pkg-config \
+    --enable-pic \
+    --enable-small \
+    --disable-iconv \
+    --disable-autodetect \
+    --disable-debug \
+    --disable-programs \
+    --disable-everything \
+    --disable-network \
+    --enable-demuxer=mov \
+    --enable-parser=h264 \
+    --enable-decoder=h264 \
